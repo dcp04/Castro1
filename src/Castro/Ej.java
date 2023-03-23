d634fcae75c6e99a45ef88b507450f26fa4e2411
davic <davic@CASTRO-ASUS>
2023-03-23 19:37:12 +0100

    Con esta version creamos la primera rama a fecha de 23 de marzo de 2023

---
+++ src/Castro/Ej.java
@@ -0,0 +1,12 @@
+package Castro;
+
+public class Ej {
+    public static void main(String[] args) {
+      int i = 0;
+      do {
+        System.out.print(" | ");
+        System.out.print(i);
+        i+=5;
+      } while(i <= 100);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author davic
+ *
+ */
+module ProyectoDavidCastro {
+}
